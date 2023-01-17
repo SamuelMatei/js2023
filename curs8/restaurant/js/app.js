b58727518efa16dfd8f585f7a60fe886645bd056
Frontend JavaScript <frontendjavascript@C3-23.local>
2023-01-17 20:55:17 +0200

    curs8

---
+++ curs8/restaurant/js/app.js
@@ -0,0 +1,82 @@
+// variabila menu este definita in data.js
+
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+    diplayMenuItems(menu);
+    displayMenuButtons();
+});
+
+function diplayMenuItems(menuItems) {
+
+}
+function displayMenuButtons() {
+    const categories = menu.reduce(
+        function (values, item) {
+            if (!values.includes(item.category)) {
+                values.push(item.category);
+            }
+            return values;
+        },
+        ["all"]
+    );
+    const categoryBtns = categories.map(function (category, index) {
+        let btn_nr = 'btn' + (index + 1);
+        const categoryRo = {
+            all: 'Tot',
+            breakfast: 'Mic dejun',
+            lunch: 'Pranz',
+            dinner: 'Cina',
+            shakes: 'Bauturi',
+        }
+        return `<button type="button" class="filter-btn"data-is=${category} . data-nr=${btn_nr}>
+        ${categoryRo[category]}
+        </button>`;
+    })
+        .join("");
+
+    btnContainer.innerHTML = categoryBtns;
+
+    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+    filterBtns.forEach(function (btn) {
+        btn.addEventListener("click", function (e) {
+            // console.log(e.currentTarget.dataset);
+            // counterClick(e.currentTarget);
+            const category = e.currentTarget.dataset.id;
+            const menuCategory = menu.filter(function (menuItem) {
+                // console.log(menuItem.category);
+                if (menuItem.category === category) {
+                    return menuItem;
+                }
+            });
+            if (category === "all") {
+                displayMenuItems(menu);
+            } else {
+                displayMenuItems(menuCategory);
+            }
+        });
+    });
+
+    function displayMenuItems(menuItems) {
+        let displayMenu = menuItems.map(function (item) {
+
+            return `<article class="menu-item">
+                <img src="./img/item-10.jpeg" alt="menu item" class="photo" />
+                <div class="item-info">
+                  <header>
+                    <h4>buttermilk pancakes</h4>
+                    <h4 class="price">15 RON</h4>
+                  </header>
+                  <p class="item-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+                    laboriosam excepturi! Quo, officia.
+                  </p>
+                </div>
+              </article>`
+        })
+    }
+
+}
